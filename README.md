ddc45fe3288dfd8b94c4f7098240518315c6416f
Logan Brock <logan.g.brock@gmail.com>
2021-08-03 08:45:09 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+<h1 align="center">Welcome to pricelist 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-0.1.0-blue.svg?cacheSeconds=2592000" />
+  <a href="#" target="_blank">
+    <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-yellow.svg" />
+  </a>
+  <a href="https://twitter.com/lifeisagarden23" target="_blank">
+    <img alt="Twitter: lifeisagarden23" src="https://img.shields.io/twitter/follow/lifeisagarden23.svg?style=social" />
+  </a>
+</p>
+
+> Price list created by using PHP software language and Composer package manager to connect to mySQL and Craft CMS 
+
+### 🏠 [Homepage](https://github.com/lgbrock/pricelist)
+
+## Install
+
+```sh
+composer install
+```
+
+## Usage
+
+```sh
+composer --version
+```
+
+## Author
+
+👤 **Logan Brock**
+
+* Website: logangbrock.com
+* Twitter: [@lifeisagarden23](https://twitter.com/lifeisagarden23)
+* Github: [@lgbrock](https://github.com/lgbrock)
+* LinkedIn: [@loganbrock](https://linkedin.com/in/loganbrock)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ Router.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace app;
+
+class Router
+{
+    public array $getRoutes = [];
+    public array $postRoutes = [];
+
+    public ?Database $database = null;
+
+    public function __construct(Database $database)
+    {
+        $this->database = $database;
+    }
+
+    public function get($url, $fn)
+    {
+        $this->getRoutes[$url] = $fn;
+    }
+
+    public function post($url, $fn)
+    {
+        $this->postRoutes[$url] = $fn;
+    }
+
+    public function resolve()
+    {
+        $method = strtolower($_SERVER['REQUEST_METHOD']);
+        $url = $_SERVER['PATH_INFO'] ?? '/';
+
+        if ($method === 'get') {
+            $fn = $this->getRoutes[$url] ?? null;
+        } else {
+            $fn = $this->postRoutes[$url] ?? null;
+        }
+        if (!$fn) {
+            echo 'Page not found';
+            exit;
+        }
+        echo call_user_func($fn, $this);
+    }
+
+    public function renderView($view, $params = [])
+    {
+        foreach ($params as $key => $value) {
+            $$key = $value;
+        }
+        ob_start();
+        include __DIR__."/views/$view.php";
+        $content = ob_get_clean();
+        include __DIR__."/views/_layout.php";
+    }
+}
